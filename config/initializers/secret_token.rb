e59388a995fa67d880dd1ed8224b276d8092fd50
Tim Thomas <tim@buyplaytix.com>
2012-01-13 14:31:15 -0600

    rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nwa::Application.config.secret_token = '43e2d319fb44d48beef11fddf245eaa617773430e35ca03f556a14dbd6bf51454c52ed420403399bd72256437d7206cafc408e7eca0568423f611a6e7d8e97a0'
