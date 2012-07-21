fabdb63cdece38bafebc2a797dcb03e5eaa6d011
Tim Thomas <tim@buyplaytix.com>
2012-07-21 14:57:56 -0500

    added url to locations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120720212037) do
+ActiveRecord::Schema.define(:version => 20120721195241) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(:version => 20120720212037) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "hidden"
+    t.string   "url"
   end
 
 end
