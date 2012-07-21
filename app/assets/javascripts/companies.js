c6513844ee2b039623342d668b398fce55c57b02
Tim Thomas <tim@buyplaytix.com>
2012-07-21 15:22:17 -0500

    fixed for tablet

--- app/assets/javascripts/companies.js
+++ app/assets/javascripts/companies.js
@@ -1,19 +1,21 @@
+function toggleOn() {
+  $(this).children('.darkener').show(function() {
+      
+  });
+  $('.description').textfill({ maxFontPixels: 22 });
+  $(this).children('.company-name').hide();
+  $(this).children('.company-name-alt').show();
+}
+function toggleOff() {
+  $(this).children('.darkener').hide();
+  $(this).children('.company-name').show();
+  $(this).children('.company-name-alt').hide();
+}
+
 $(function() {
   $('.company-name').textfill({ maxFontPixels: 12 });
   
   
-  $('.menu-img').hover(function() {
-      $(this).children('.darkener').show(function() {
-          
-      });
-      $('.description').textfill({ maxFontPixels: 22 });
-      $(this).children('.company-name').hide();
-      $(this).children('.company-name-alt').show();
-    },
-    function() {
-      $(this).children('.darkener').hide();
-      $(this).children('.company-name').show();
-      $(this).children('.company-name-alt').hide();
-    }
-  );
+  $('.menu-img').hover(toggleOn, toggleOff);
+  $('.menu-img').toggle(toggleOn, toggleOff);
 });
